3b649a04e7a2bd1f064f5cbe39a88b7041098edb
Daishi Kaszer <daishi.kaszer@yahoo.fr>
2015-01-14 16:45:17 +0100

    Ajout d'exos, de commentaires dans les sources et d'un recap des dossiers code et exos

--- exos/fonction-exo9-correction.php
+++ exos/fonction-exo9-correction.php
@@ -14,7 +14,7 @@ function a_plus_grand_que_b($a, $b) {
     }
 }
 
-// deuxième implémentation possible
+// autre implémentation possible
 // function a_plus_grand_b($a, $b) {
 //     if (!is_int($a) || !is_int($b)) {
 //         return false;
@@ -29,21 +29,21 @@ function a_plus_grand_que_b($a, $b) {
 //     }
 // }
 
-$resultat = nombres_entiers(10, 5);
+$resultat = a_plus_grand_que_b(10, 5);
 var_dump($resultat);
 
-$resultat = nombres_entiers(2, 8);
+$resultat = a_plus_grand_que_b(2, 8);
 var_dump($resultat);
 
-$resultat = nombres_entiers(7, 7);
+$resultat = a_plus_grand_que_b(7, 7);
 var_dump($resultat);
 
-$resultat = nombres_entiers('foo', 5);
+$resultat = a_plus_grand_que_b('foo', 5);
 var_dump($resultat);
 
-$resultat = nombres_entiers(10, 'bar');
+$resultat = a_plus_grand_que_b(10, 'bar');
 var_dump($resultat);
 
-$resultat = nombres_entiers('foo', 'bar');
+$resultat = a_plus_grand_que_b('foo', 'bar');
 var_dump($resultat);
 
